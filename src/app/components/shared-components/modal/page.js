530fbca90eba1b6b00352a54115fb44d831afdba
rohanawan <rohan.awan863@gmail.com>
2023-06-16 00:55:00 +0500

    Subject: Refactor code

--- src/app/components/shared-components/modal/page.js
+++ src/app/components/shared-components/modal/page.js
@@ -41,9 +41,9 @@ const Modal = ({open, setOpen, setIsUserCreated}) => {
     }
   })
   return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={() => setOpen(false)}>
-        <Transition.Child
+    <div show={open}>
+      <Dialog as="div" className="relative z-10" open={open || false} initialFocus={cancelButtonRef} onClose={() => setOpen(false)}>
+        <div
           show={open}
           as={Fragment}
           enter="ease-out duration-300"
@@ -54,11 +54,11 @@ const Modal = ({open, setOpen, setIsUserCreated}) => {
           leaveTo="opacity-0"
         >
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
+        </div>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <Transition.Child
+            <div
               as={Fragment}
               show={open}
               enter="ease-out duration-300"
@@ -69,7 +69,7 @@ const Modal = ({open, setOpen, setIsUserCreated}) => {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 width-modal">
                   <div className="sm:items-start">
                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                     <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-gray-100 mb-4">
@@ -174,11 +174,11 @@ const Modal = ({open, setOpen, setIsUserCreated}) => {
                   </div>
                 </div>
               </Dialog.Panel>
-            </Transition.Child>
+            </div>
           </div>
         </div>
       </Dialog>
-    </Transition.Root>
+    </div>
   );
 }
 
